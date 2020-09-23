0af48de1daf9cc2fd0e027ddf192358c29aa1025
yaobj <yaobj@wjs.com>
2020-09-23 15:39:56 +0800

    代码提交

--- src/main/java/warrior/bean/HelloWorld.java
+++ src/main/java/warrior/bean/HelloWorld.java
@@ -1,9 +1,17 @@
 package warrior.bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class HelloWorld {
 
-	public void hello() {
+	@Autowired
+	private Person person;
 
-		System.out.println("hello world !!!");
+	public void hello(String name) {
+
+		System.out.println(person.toString());
+		System.out.println("hello world !!!" + name);
 	}
 }
--- src/test/java/warrior/MainTest.java
+++ src/test/java/warrior/MainTest.java
@@ -1,5 +1,13 @@
 package warrior;
 
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,9 +18,67 @@ public class MainTest {
 	private static ApplicationContext applicationContext;
 
 	public static void main(String[] args) {
+
 		applicationContext = new ClassPathXmlApplicationContext("spring-applicationContext.xml");
 		HelloWorld helloWorld = applicationContext.getBean(HelloWorld.class);
-		helloWorld.hello();
+		helloWorld.hello("孙悟空");
+		DataSource dataSource = new DataSource() {
+
+			public PrintWriter getLogWriter() throws SQLException {
+
+				// TODO 补充自动生成的代码块
+				return null;
+			}
+
+			public void setLogWriter(PrintWriter out) throws SQLException {
+
+				// TODO 补充自动生成的代码块
+
+			}
+
+			public void setLoginTimeout(int seconds) throws SQLException {
+
+				// TODO 补充自动生成的代码块
+
+			}
+
+			public int getLoginTimeout() throws SQLException {
+
+				// TODO 补充自动生成的代码块
+				return 0;
+			}
+
+			public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+
+				// TODO 补充自动生成的代码块
+				return null;
+			}
+
+			public <T> T unwrap(Class<T> iface) throws SQLException {
+
+				// TODO 补充自动生成的代码块
+				return null;
+			}
+
+			public boolean isWrapperFor(Class<?> iface) throws SQLException {
+
+				// TODO 补充自动生成的代码块
+				return false;
+			}
+
+			public Connection getConnection() throws SQLException {
+
+				// TODO 补充自动生成的代码块
+				return null;
+			}
+
+			public Connection getConnection(String username, String password) throws SQLException {
+
+				// TODO 补充自动生成的代码块
+				return null;
+			}
+
+		};
 	}
 
 }
